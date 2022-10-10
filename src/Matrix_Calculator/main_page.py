3abda3f08dd985d52aa0662af242b597167d72a3
AlkTagar <r0gozinmax@yandex.ru>
2022-10-10 17:51:47 +0300

    base of bot

--- src/Matrix_Calculator/main_page.py
+++ src/Matrix_Calculator/main_page.py
@@ -1,38 +1,16 @@
-import numpy as np
+import telebot
 from rich.console import Console
 
-import User
-import texts
-import Add_matrixes
+import Config
 
 
+bot = telebot.TeleBot(Config.TOKEN)
 console = Console()
-matrixes = {}
 
 
-console.print(f"{texts.start}", style="red")
-
-def main():
-    console.print(f"{texts.man}", style="blue")
-    comand = console.input("[bold green]=> ")
-
-    match comand:
-        case "0":
-            Add_matrixes()
-
-        case "3":
-            pass
-
-        case "h":
-            pass
-
-        case "q":
-            exit()
-
-        case _:
-            console.print("[bold red] Команда не обнаружена")
+console.log("[bold green]START BOT")
 
 
-while True:
-    main()
-
+@bot.message_handler(content_types=["text"])
+def main_handler():
+    pass
