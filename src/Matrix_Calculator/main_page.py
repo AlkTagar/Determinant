2dc571cbb152d628f9e6da3ec2489690c4e4d238
AlkTagar <r0gozinmax@yandex.ru>
2022-10-12 06:37:57 +0300

    add new messages and start of systems

--- src/Matrix_Calculator/main_page.py
+++ src/Matrix_Calculator/main_page.py
@@ -1,27 +1,35 @@
+from rich import style
 import telebot
 from rich.console import Console
 
 import Config
 import Systems
+import texts
 
 
 bot = telebot.TeleBot(Config.TOKEN)
 console = Console()
 
 
-console.log("[bold green]START BOT")
+console.log("START BOT", style="bold green")
 
 
-@bot.message_handler(content_types=["text"])
+@bot.message_handler()
 def main_handler(message):
     match message.text:
 
         case "/start":
-            bot.send_message(message.from_user.id, "Start bot")
-            console.log("[cyan]get \"start\"")
+            bot.send_message(message.from_user.id, texts.start)
+            console.log("get \"start\"", style="italic cyan")
 
         case "/new_system":
-            pass
+            bot.send_message(message.from_user.id, texts.system)
+            console.log("new system", style="italic blue")
+
+        case _:
+            bot.send_message(message.from_user.id, "?")
+            console.log("unknown message", style="italic yellow")
 
 
 bot.infinity_polling()
+
